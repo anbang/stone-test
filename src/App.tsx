60f979079d8ce7129e2431d5c738201ca2a728b6
Anbang <anbang.zhu@definer.org>
2022-04-28 02:15:53 +0800

    Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="App">
+      <header id="header">
+        <div className="container">
+          <div className="logo">logo</div>
+          <div className="search">search</div>
+          <div className="topbar">topbar</div>
+        </div>
+      </header>
+      <div className="container">
+        <div id="left-sidebar">left-sidebar</div>
+        <div id="content">
+          <div id="mainbar">mainbar</div>
+          <div id="sidebar"> sidebar</div>
+        </div>
+      </div>
+      <header id="footer">
+        <div className="container">Footer</div>
       </header>
     </div>
   );
